163c91dc18e35a4b61f3a90b2317cb3de5023f89
Briton Otieno <ojbrot@gmail.com>
2025-08-06 12:12:28 +0300

    Add the tags controller

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::TagsController < ApplicationController
+  def index
+    tags = Tag.all
+    render json: tags
+  end
+
+  def create
+    tag = Tag.new(tag_params)
+    if tag.save
+      render json: tag, status: :created
+    else
+      render json: tag.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name, :count)
+  end
 end
